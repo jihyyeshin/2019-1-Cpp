e660678b9084ed8227fcf895a6d75a973566d650
jihyyeshin <tlsjh082@gmail.com>
2019-03-17 23:40:48 +0900

    190317

---
+++ 2주차/2.cpp
@@ -0,0 +1,20 @@
+///*
+//실습문제02
+//
+//학과: 컴퓨터공학과
+//학번: 2016112127
+//이름: 신지혜
+//*/
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	for (int i = 1; i < 10; i++) {
+//		for (int j = 1; j < 10; j++) 
+//			cout << i <<'*' << j << '=' << i*j << '\t';
+//		cout << "\n";
+//	}
+//	return 0;
+//
+//}
