77274180eec419c192001759bff6bd6a4604cfbc
jihyyeshin <tlsjh082@gmail.com>
2019-03-17 23:59:28 +0900

    final6

--- 2주차/6.cpp
+++ 2주차/6.cpp
@@ -6,9 +6,20 @@
 이름: 신지혜
 */
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(void) {
+	string passW1, passW2;
+	
+	cout << "새 암호를 입력하세요 >>";
+	cin >>passW1;
+	cout << "새 암호를 다시 한 번입력하세요 >>";
+	cin >> passW2;
+	if (passW1.compare(passW2) == 0)
+		cout << "같습니다\n";
+	else
+		cout << "같지 않습니다\n";
 
 	return 0;
 }
