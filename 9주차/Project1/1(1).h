464a246a4291d51c4fd8d0bdc4b57f16b56b48ec
jihyyeshin <tlsjh082@gmail.com>
2019-05-04 18:45:54 +0900

    190504 ~num1

---
+++ 9주차/Project1/1(1).h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book {
+	string title;
+	int price;
+	int pages;
+public:
+	Book(string title = "", int price = 0, int pages = 0) {
+		this->title = title; this->price = price; 	this->pages = pages;
+	}
+	void show() {
+		cout << title << ' ' << price << "원 " << pages << " 페이지" << endl;
+	}
+	string getTitle() { return title; }
+	Book& operator +=(int price);
+	Book& operator -=(int price);
+};
+
+Book& Book::operator+=(int price) {
+	this->price += price;
+	return *this;
+}
+
+Book& Book::operator-=(int price) {
+	this->price -= price;
+	return *this;
+}
